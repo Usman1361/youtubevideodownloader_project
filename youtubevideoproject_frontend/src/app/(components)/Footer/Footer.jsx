0e9d377bf2797eb55b3b8c4512981e19efafe4ef
Muhammad Usman <112246337+Usman1361@users.noreply.github.com>
2024-01-29 22:02:27 +0500

    Update Footer.jsx

--- youtubevideoproject_frontend/src/app/(components)/Footer/Footer.jsx
+++ youtubevideoproject_frontend/src/app/(components)/Footer/Footer.jsx
@@ -1,14 +1,24 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import React from "react";
-
+import LanguageIcon from "@mui/icons-material/Language";
 const Footer = () => {
+  const ownerData = [
+    {
+      name: "Muhammad Usman",
+      link: "https://usman-portfolio-app.netlify.app/",
+    },
+    {
+      name: "Muhammad Haroon",
+      link: "https://codexharoon.com/",
+    },
+  ];
   return (
     <>
       <Box sx={{ backgroundColor: "#242B2E", color: "white" }}>
         <Container>
           <Grid container>
             <Grid item xs={12} md={4}>
-              <Typography variant="h5" fontFamily="Poppins">
+              <Typography pt={4} pb={3} variant="h5" fontFamily="Poppins">
                 Alldownload4u
               </Typography>
               <Typography variant="body2" fontFamily="Poppins">
@@ -16,7 +26,29 @@ const Footer = () => {
                 can download any youtube video in any quality.
               </Typography>
             </Grid>
-            <Grid item xs={12} md={4}></Grid>
+            <Grid item pl={3} xs={12} md={4}>
+              <Typography pt={4} pb={3} variant="h5" fontFamily="Poppins">
+                Contact Us
+              </Typography>
+              <Box sx={{ display: "flex" }}>
+                {ownerData.map((item) => (
+                  <>
+                    <Button
+                      variant="outlined"
+                      fontFamily="Poppins"
+                      href={item.link}
+                      sx={{ marginRight: "10px" }}
+                    >
+                      <LanguageIcon />
+                    </Button>
+                  </>
+                ))}
+              </Box>
+
+              <Typography variant="body2" fontFamily="Poppins">
+                Email:
+              </Typography>
+            </Grid>
             <Grid item xs={12} md={4}></Grid>
           </Grid>
         </Container>
